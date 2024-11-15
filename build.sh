413777b125f1a506de27f145eb314d33092ee769
korrinton <ramonberzosa@hotmail.com>
2024-11-15 18:50:43 +0100

    [subido el build con el css]

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# Directorio donde se descargará w3.css
+css_dir="css"
+mkdir "css"
+mkdir "Web"
+
+
+wget https://www.w3schools.com/w3css/4/w3.css -P "$css" -O "./css/w3.css"
+
 echo "creando la página principal"
+
 ./index.sh
 echo "Creando página 1."
+touch ./Web/pagina1.html
 ./pagina1.sh
 echo "Creando página 2."
+touch ./Web/pagina1.html
 ./pagina2.sh
 echo "Creando página 3."
+touch ./Web/pagina1.html
 ./pagina3.sh
-cat <<EOF
-Páginas creadas.
-Puede encontrar las páginas web dentro de la carpeta Web.
-EOF
+
--- cabecera.sh
+++ cabecera.sh
@@ -8,11 +8,12 @@ cat << EOF
 <head>
   <meta charset='utf-8'>
   <title> $1 </title>
+   <link rel="stylesheet" href="css/w3.css">
 </head>
 <body>
 <h1>Esta es la $1.</h1>
 
-<h1 style="text-align: center;">Ejercicio de repositorio de github</h1>
+<h1 style="text-align: center;">Ejercicio de repositorio de gith y Jenkins</h1>
 </body>
-</html>
+
 EOF
